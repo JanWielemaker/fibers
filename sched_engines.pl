b537c2ddcb0254a9a3c97031c9b8e6d0bf98776b
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-03-05 16:07:35 +0100

    New design

--- sched_engines.pl
+++ sched_engines.pl
@@ -1,48 +1,73 @@
-:- use_module(library(apply)).
+:- module(sched_engines,
+	  [ run_in_fiber/1,	% :Goal
+	    set_immediate/1	% :Goal
+	  ]).
+
+/** <module> Engine based cooperative multi tasking
+
+*/
+
 :- use_module(library(debug)).
 
 :- meta_predicate
        run_in_fiber(0).
 
-:- dynamic
-       schedule/1.
+%!  run_in_fiber(:Goal)
+%
+%   Run Goal in a fiber.
 
 run_in_fiber(Goal) :-
-    term_variables(Goal, Vars),
-    Templ =.. [v|Vars],
-    engine_create(Templ, engine_run(Goal), Engine),
-    asserta(schedule(Engine)),
+    engine_create(true, engine_run(Goal), Engine),
     event_loop([Engine]).
 
 engine_run(Goal) :-
     set_prolog_flag(heartbeat, 1000),
     call(Goal).
 
+%!  engine_run(+Engines) is det.
+%
+%   Run the event loop until all engines have completed
+
 event_loop([]) :-
     !.
 event_loop(Engines) :-
-    convlist(advance, Engines, Engines1),
-    event_loop(Engines1).
+    phrase(advance_tasks(Engines), NewEngines),
+    event_loop(NewEngines).
 
-advance(Engine, Engine) :-
-    engine_next_reified(Engine, Result),
+advance_tasks([]) -->
+    [].
+advance_tasks([H|T]) -->
+    advance(H),
+    advance_tasks(T).
+
+advance(Engine) -->
+    { engine_next_reified(Engine, Result) },
     consume(Result, Engine).
 
-consume(the(Answer), Engine) =>
-    debug(event_loop(answer), 'Got ~p from ~p', [Answer, Engine]).
-consume(no, Engine) =>
-    debug(event_loop(complete), 'Engine ~p failed', [Engine]),
-    fail.
-consume(exeption(Error), _Engine) =>
-    print_message(error, Error),
-    fail.
+consume(the(heartbeat), From) -->
+    [From].
+consume(the(schedule(New)), [From]) -->
+    [From,New].
+consume(the(true), _From) -->
+    [].
+consume(no, _From) -->
+    [].
+consume(exeption(Error), _From) -->
+    { print_message(error, Error) },
+    [].
+
+%!  set_immediate(:Goal)
+%
+%   Create a new fiber for Goal.
+
+set_immediate(Goal) :-
+    engine_create(true, engine_run(Goal), Engine),
+    engine_yield(schedule(Engine)).
 
 prolog:heartbeat :-
-    thread_self(Me),
     (   is_async
-    ->  format(user_error, '~p: heartbeat~n', [Me]),
-	engine_yield(heartbeat)
-    ;   format(user_error, '~p: ignored heartbeat~n', [Me])
+    ->  engine_yield(heartbeat)
+    ;   true
     ).
 
 is_async :-
